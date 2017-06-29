ff0a3af985e2bd10312623fadf32d67760e735f8
Jacob Ulrich <julrich@gmail.com>
2017-06-29 13:21:48 -0700

    Minor fixes

--- js/carousel.js
+++ js/carousel.js
@@ -1,4 +1,5 @@
 let slideIndex = 1;
+let slideTimer = null;
 // showSlides(slideIndex);
 
 function plusSlides(n) {
@@ -9,30 +10,38 @@ function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
-// function showSlides(n) {
-//   var i;
-//   var slides = document.getElementsByClassName("mySlides");
-//   var dots = document.getElementsByClassName("dot");
-//   if (n > slides.length) {slideIndex = 1;}
-//   if (n < 1) {slideIndex = slides.length;}
-//   for (i = 0; i < slides.length; i++) {
-//       slides[i].style.display = "none";
-//   }
-//   for (i = 0; i < dots.length; i++) {
-//       dots[i].className = dots[i].className.replace(" active", "");
-//   }
-//   slides[slideIndex-1].style.display = "block";
-//   dots[slideIndex-1].className += " active";
-// }
+function showSlides(n) {
+  clearInterval(slideTimer);
+  
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
 
-function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex> slides.length) {slideIndex = 1;}
-    slides[slideIndex-1].style.display = "block";
-    setTimeout(showSlides, 3000); // Change image every 2 seconds
+  if (n > slides.length) {slideIndex = 1;}
+  if (n < 1) {slideIndex = slides.length;}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  if (slideIndex> slides.length) {slideIndex = 1;}
+
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+
+  slideIndex++;
+  slideTimer = setInterval(showSlides, 3000);
 }
+
+// function showSlidesTimer() {
+//     var i;
+//     var slides = document.getElementsByClassName("mySlides");
+//     for (i = 0; i < slides.length; i++) {
+//         slides[i].style.display = "none";
+//     }
+//     slideIndex++;
+//     if (slideIndex> slides.length) {slideIndex = 1;}
+//     slides[slideIndex-1].style.display = "block";
+//     slideTimer = setTimeout(showSlides, 3000);
+// }
